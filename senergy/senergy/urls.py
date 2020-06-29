c741bdf37a8997cb05693c571a9e7a9664bfb7a6
YeonGyu Kim <public.kim.yeon.gyu@gmail.com>
2020-06-30 02:11:25 +0900

    Add comments

--- senergy/senergy/urls.py
+++ senergy/senergy/urls.py
@@ -22,9 +22,12 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-    openapi.Info(title="Senergy API",
-                 default_version='v1',
-                 description="The api documentations of senergy"),
+    openapi.Info(
+        title="Senergy API",
+        default_version='v1',
+        description=
+        """The api documentations of senergy. Uses Basic Auth for authentication process."""
+    ),
     validators=['flex'],
     public=True,
     permission_classes=(permissions.AllowAny, ),
